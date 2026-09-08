c306640657628a83d78e45e87d7b3f0f0cfa5b7e
Dmitrii Mikhailov <dmikhail@student.42porto.com>
2026-09-08 17:08:18 +0100

    pac-man.py

---
+++ pac-man.py
@@ -0,0 +1,16 @@
+from mazegenerator import MazeGenerator
+
+mazegen = MazeGenerator((15, 15), False, (0, 0), (10, 10), 42)
+
+if __name__ == "__main__":
+    # mazegen.generate()
+    maze = mazegen.maze
+    for x in maze:
+        print(x)
+    print()
+    print(mazegen._shortest_path)
+
+    mazegen._entryx, mazegen._entryy = 1, 1
+    mazegen._exitx, mazegen._exity = 2, 2
+    mazegen._find_short_path()
+    print(mazegen._shortest_path)
