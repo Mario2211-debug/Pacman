29099f8b0d255c87e2db94babfbd7435d27d9f32
Dmitrii Mikhailov <dmikhail@student.42porto.com>
2026-09-09 18:37:52 +0100

    Pathfinding improved

--- pac-man.py
+++ pac-man.py
@@ -13,22 +13,44 @@ if __name__ == "__main__":
     cfg = Config.model_validate(config_json)
     # print(cfg)
 
+
+    ghost1 = Ghost()
+    ghost2 = Ghost()
+    ghost3 = Ghost()
+    ghost4 = Ghost()
+
     for lvl in cfg.level:
         mazegen = MazeGenerator((lvl["width"], lvl["height"]), False, (0, 0), (1, 1), cfg.seed)
         for x in mazegen.maze:
             print(x)
 
-        ghost1 = Ghost(None, 0, 0)
-        ghost2 = Ghost(None, lvl["width"] - 1, 0)
-        ghost3 = Ghost(None, 0, lvl["height"] - 1)
-        ghost4 = Ghost(None, lvl["width"] - 1, lvl["height"] - 1)
-        print("Ghost 1:", ghost1.x, ghost1.y)
-        ghost1.find_path(mazegen.maze, 3, 3)
-        print("Ghost 2:", ghost2.x, ghost2.y)
-        ghost2.find_path(mazegen.maze, 3, 3)
-        print("Ghost 3:", ghost3.x, ghost3.y)
-        ghost3.find_path(mazegen.maze, 3, 3)
-        print("Ghost 4:", ghost4.x, ghost4.y)
-        ghost4.find_path(mazegen.maze, 3, 3)
+        ghosts_positions:list = []
+        ghost1.set_maze(mazegen.maze)
+        ghost1.set_position(0, 0)
+        print("\nGhost 1 position:", ghost1.x, ghost1.y)
+        next_position = ghost1.find_movement_to(3, 3)
+        ghosts_positions.append(next_position)
+        print("Next position:", next_position)
+
+        ghost2.set_maze(mazegen.maze)
+        ghost2.set_position(lvl["width"] - 1, 0)
+        print("\nGhost 2 position:", ghost2.x, ghost2.y)
+        next_position = ghost2.find_movement_to(3, 3, ghosts_positions)
+        ghosts_positions.append(next_position)
+        print("Next position:", next_position)
+
+        ghost3.set_maze(mazegen.maze)
+        ghost3.set_position(0, lvl["height"] - 1)
+        print("\nGhost 3 position:", ghost3.x, ghost3.y)
+        next_position = ghost3.find_movement_to(3, 3, ghosts_positions)
+        ghosts_positions.append(next_position)
+        print("Next position:", next_position)
+
+        ghost4.set_maze(mazegen.maze)
+        ghost4.set_position(lvl["width"] - 1, lvl["height"] - 1)
+        print("\nGhost 4 position:", ghost4.x, ghost4.y)
+        next_position = ghost4.find_movement_to(3, 3, ghosts_positions)
+        print("Next position:", next_position)
 
         print()
+        break
--- src/ghost.py
+++ src/ghost.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 class Ghost:
-    def __init__(self, image: int = None, x: int = 0, y: int = 0):
+    def __init__(self, image: int = 0, x: int = 0, y: int = 0, maze: list[list[int]] = []) -> None:
         self.image = image
         self.x = x
         self.y = y
@@ -11,35 +11,58 @@ class Ghost:
         self.y_px = y * 10
         self.edible = False
         self.freeze = False
+        self._maze = maze
+        if maze:
+            self._maze_width = len(maze[0])
+            self._maze_height = len(maze)
 
-    def find_path(self, maze:list[list[int]], end_x: int, end_y: int) -> None:
-        moves = [(0, -1, 1, 'N'), (1, 0, 2, 'E'),
-                 (0, 1, 4, 'S'), (-1, 0, 8, 'W')]   # dx, dy, wall code, letter
+    def set_maze(self, maze: list[list[int]]) -> None:
+        self._maze = maze
+        if maze:
+            self._maze_width = len(maze[0])
+            self._maze_height = len(maze)
+
+    def set_position(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+        self.x_px = x * 10
+        self.y_px = y * 10
+
+    def find_movement_to(self, end_x: int, end_y: int, ghosts_positions: list[tuple[int]] = []) -> tuple[int]:
+        moves = [(0, -1, 1), (1, 0, 2),
+                 (0, 1, 4), (-1, 0, 8)]
         start = (self.x, self.y)
         goal = (end_x, end_y)
         prev: dict = {start: None}
         queue = deque([start])
-        maze_width = len(maze[0])
-        maze_height = len(maze)
         while queue:
             x, y = queue.popleft()
             if (x, y) == goal:
                 break
-            for dx, dy, code, letter in moves:
+            for dx, dy, code in moves:
                 nx, ny = x + dx, y + dy
-                if (0 <= nx < maze_width and 0 <= ny < maze_height
-                        and (maze[y][x] & code) == 0
+                if (nx, ny) in ghosts_positions:
+                    continue
+                if (0 <= nx < self._maze_width and 0 <= ny < self._maze_height
+                        and (self._maze[y][x] & code) == 0
                         and (nx, ny) not in prev):
-                    prev[(nx, ny)] = ((x, y), letter)
+                    prev[(nx, ny)] = ((x, y))
                     queue.append((nx, ny))
         if goal not in prev:
-            print("MazeGenerator Class error: no shortest path found.")
-            return
-        letters = []
+            # print("MazeGenerator Class error: no shortest path found.")
+            # return
+            return (self.x, self.y)
+        parents = []
         cur = goal
         while prev[cur] is not None:
-            parent, letter = prev[cur]
-            letters.append(letter)
+            parent = prev[cur]
+            parents.append(parent)
             cur = parent
-        print(''.join(reversed(letters)))
-        return
+        if parents:
+            self.next_x = parents[-2][0]
+            self.next_y = parents[-2][1]
+            return parents[-2]
+        else:
+            self.next_x = goal[0]
+            self.next_y = goal[1]
+            return goal
