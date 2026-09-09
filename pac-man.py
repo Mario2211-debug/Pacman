5b5029c713cbf423175ca1509b6ddb2ed6be1e59
Dmitrii Mikhailov <dmikhail@student.42porto.com>
2026-09-09 15:10:18 +0100

    Config validation

--- pac-man.py
+++ pac-man.py
@@ -2,22 +2,22 @@ import sys
 import json
 
 from mazegenerator import MazeGenerator
-# from src.parser import read_json_file
+from src.config import Config
 
 mazegen = MazeGenerator((15, 15), False, (0, 0), (10, 10), 42)
 
 if __name__ == "__main__":
     # mazegen.generate()
     maze = mazegen.maze
-    for x in maze:
-        print(x)
-    print()
-    print(mazegen._shortest_path)
+    # for x in maze:
+        # print(x)
+    # print()
+    # print(mazegen._shortest_path)
 
     mazegen._entryx, mazegen._entryy = 1, 1
     mazegen._exitx, mazegen._exity = 2, 2
     mazegen._find_short_path()
-    print(mazegen._shortest_path)
+    # print(mazegen._shortest_path)
 
     if len(sys.argv) == 2:
         try:
@@ -25,6 +25,12 @@ if __name__ == "__main__":
                 config_content = f.readlines()
                 lines = "".join(line for line in config_content if not line.lstrip().startswith('#'))
                 json_lines = json.loads(lines)
-                print(json_lines)
+                # print(json_lines)
+                cfg = Config.model_validate(json_lines)
+                print()
+                print()
+                print(cfg)
+
         except Exception as err:
-            print(err)
+            print("ERROR")
+            print(f"\033[91m{err}\033[0m")
--- src/config.py
+++ src/config.py
@@ -1,17 +1,71 @@
-from pydantic import BaseModel, WrapValidator
-from typing import Annotated
+from pydantic import BaseModel, Field, ValidationInfo, model_validator
+from pydantic.functional_validators import WrapValidator
+from typing import Any, Annotated
 
 
+CONFIG_DEFAULTS = {
+    "highscore_filename": "highscore.json",
+    "lives": 3,
+    "pacgum": 42,
+    "points_per_pacgum": 10,
+    "points_per_super_pacgum": 50,
+    "points_per_ghost": 200,
+    "seed": 42,
+    "level": [
+        {"width": 15, "height": 15},
+        {"width": 20, "height": 15},
+        {"width": 30, "height": 20},
+        {"width": 35, "height": 25},
+        {"width": 25, "height": 15}
+        ]
+    }
 
-class Config:
-    def __init__(self):
-        self.highscore_filename = "highscore.json"
-        self.level = [1, 2, 3, 4, 5]
-        self.width = [15, 20, 30, 25, 10]
-        self.height = [15, 20, 30, 25, 10]
-        self.lives = 3
-        self.pacgum= 42
-        self.points_per_pacgum = 10
-        self.points_per_super_pacgum = 50
-        self.points_per_ghost = 200
-        self.seed = 42
+def validate_config_fields(value: Any, handler, info: ValidationInfo):
+    try:
+        return handler(value)
+    except ValueError:
+        if value is None:
+            print(f"\033[91mMissed value for \"{info.field_name}\"")
+        else:
+            print(f"\033[91mInvalid value for \"{info.field_name}\": {value}.")
+        print(f"Using default: {CONFIG_DEFAULTS.get(info.field_name)}.\033[0m")
+        return CONFIG_DEFAULTS.get(info.field_name)
+
+class Config(BaseModel):
+    highscore_filename: Annotated[str, Field(min_length=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    level: Annotated[list[dict[str, int]], Field(min_length=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    lives: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    pacgum: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    points_per_pacgum: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    points_per_super_pacgum: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    points_per_ghost: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+    seed: Annotated[int, Field(ge=1, default=None, validate_default=True), WrapValidator(validate_config_fields)]
+
+    @model_validator(mode='after')
+    def check_level_list(self) -> "Config":
+        i = 0
+        while i < len(self.level):
+            if self.level[i].get("width") is None and self.level[i].get("height") is None:
+                self.level.pop(i)
+                continue
+
+            if self.level[i].get("width") is None:
+                print(f'\033[91mMissed value "width" for level {i + 1}')
+                print(f'Using default: 10.\033[0m')
+                self.level[i]["width"] = 10
+            elif self.level[i].get("width") < 5:
+                print(f'\033[91mValue "width" for level {i + 1} is too small.')
+                print(f'Using default: 10.\033[0m')
+                self.level[i]["width"] = 10
+
+            if self.level[i].get("height") is None:
+                print(f'\033[91mMissed value "height" for level {i + 1}')
+                print(f'Using default: 10.\033[0m')
+                self.level[i]["height"] = 10
+            elif self.level[i].get("height") < 5:
+                print(f'\033[91mValue "height" for level {i + 1} is too small.')
+                print(f'Using default: 10.\033[0m')
+                self.level[i]["height"] = 10
+
+            i += 1
+        return self
