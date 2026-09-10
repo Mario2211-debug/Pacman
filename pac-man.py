5699a195c2d705e67820ba352a8c71284bfad51a
Dmitrii Mikhailov <dmikhail@student.42porto.com>
2026-09-10 14:06:15 +0100

    Main updated

--- pac-man.py
+++ pac-man.py
@@ -9,9 +9,9 @@ from src.pacman import PacMan
 from src.pacgum import pacgums_generate
 
 
-# engine = Engine(800, 600)
-# engine.set_scene(MenuScene(engine))
-# engine.run()
+engine = Engine(800, 600)
+engine.set_scene(MenuScene(engine))
+engine.run()
 
 if __name__ == "__main__":
     config_filename = ""
@@ -20,11 +20,6 @@ if __name__ == "__main__":
     config_json = open_config_file(config_filename)
     cfg = Config.model_validate(config_json)
     # print(cfg)
-
-    # ghost1 = Ghost()
-    # ghost2 = Ghost()
-    # ghost3 = Ghost()
-    # ghost4 = Ghost()
 
     ghosts = [Ghost(1), Ghost(2), Ghost(3), Ghost(4)]
     pacman = PacMan(10, 10)
@@ -57,34 +52,6 @@ if __name__ == "__main__":
                 print(f"\nGhost {ghost.image} position: {ghost.x}, {ghost.y}")
                 # print("Next position:", next_position)
             print("------------")
-
-        # ghosts_positions: list = []
-        # ghost1.set_maze(mazegen.maze)
-        # ghost1.set_start_position(0, 0)
-        # print("\nGhost 1 position:", ghost1.x, ghost1.y)
-        # next_position = ghost1.find_movement_to(3, 3)
-        # ghosts_positions.append(next_position)
-        # print("Next position:", next_position)
-
-        # ghost2.set_maze(mazegen.maze)
-        # ghost2.set_start_position(lvl["width"] - 1, 0)
-        # print("\nGhost 2 position:", ghost2.x, ghost2.y)
-        # next_position = ghost2.find_movement_to(3, 3, ghosts_positions)
-        # ghosts_positions.append(next_position)
-        # print("Next position:", next_position)
-
-        # ghost3.set_maze(mazegen.maze)
-        # ghost3.set_start_position(0, lvl["height"] - 1)
-        # print("\nGhost 3 position:", ghost3.x, ghost3.y)
-        # next_position = ghost3.find_movement_to(3, 3, ghosts_positions)
-        # ghosts_positions.append(next_position)
-        # print("Next position:", next_position)
-
-        # ghost4.set_maze(mazegen.maze)
-        # ghost4.set_start_position(lvl["width"] - 1, lvl["height"] - 1)
-        # print("\nGhost 4 position:", ghost4.x, ghost4.y)
-        # next_position = ghost4.find_movement_to(3, 3, ghosts_positions)
-        # print("Next position:", next_position)
 
         print()
         break
